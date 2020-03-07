7fb350c37662a14f1bc500b834f1c77088948578
tcracked <alkoboy90@gmail.com>
2020-03-07 15:24:19 +0300

    df

--- search/Main.java
+++ search/Main.java
@@ -1,11 +1,20 @@
 package search;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] arr = sc.nextLine().split(" ");
+        System.out.println("Enter the number of people:");
+        int num = Integer.parseInt(sc.nextLine());
+        String[][] arr = new String[num][3];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextLine().split(" ");
+        }
+        System.out.println("Enter the number of search queries:");
+
         String toFind = sc.nextLine();
         boolean flag = false;
         for (int i = 0; i < arr.length; i++) {
